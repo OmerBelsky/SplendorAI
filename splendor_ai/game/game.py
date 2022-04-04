80109188ad903cdb2f0dd69ac065fb81046ca43d
Omer Belsky <omerbelsky3@gmail.com>
2022-04-05 00:17:51 +0300

    added function to take 2 coins from same type

--- splendor_ai/game/game.py
+++ splendor_ai/game/game.py
@@ -18,7 +18,7 @@ class Game:
         self.board = Board(self.num_players)
         self.player_turn = 0
 
-    def take_three_coins(self, player, colors, colors_to_return=None):
+    def take_three_coins(self, player, colors_to_take, colors_to_return=None):
 
         if colors_to_return is None:
             colors_to_return = dict()
@@ -26,25 +26,25 @@ class Game:
         if player != self.players[self.player_turn]:
             raise ValueError("It isn't this players turn")
 
-        if sum(colors.values()) > 3:
+        if sum(colors_to_take.values()) > 3:
             raise ValueError("At most 3 coins can be requested")
 
-        if any([amnt > 1 for amnt in colors.values()]):
+        if any([amnt > 1 for amnt in colors_to_take.values()]):
             raise ValueError("Coins requested must be unique")
 
-        if not all([self.board.coins[color] > 0 for color in colors]):
+        if not all([self.board.coins[color] > 0 for color in colors_to_take]):
             raise ValueError("One or more of the colors you requested is not available on the board")
 
         if not all([player.currency[color] >= amnt for color, amnt in colors_to_return.items()]):
             raise ValueError("You do no own one or more of the colors you tried to return")
 
-        if len(player.currency) + len(colors) - len(colors_to_return) > 10:
+        if len(player.currency) + len(colors_to_take) - len(colors_to_return) > 10:
             raise ValueError("Your action brings you to more than 10 coins")
 
-        if GemColor.JOKER in colors:
+        if GemColor.JOKER in colors_to_take:
             raise ValueError("Jokers can only be taken via mortgaging")
 
-        for color in colors:
+        for color in colors_to_take:
             player.currency[color] += 1
             self.board.coins[color] -= 1
 
@@ -52,3 +52,28 @@ class Game:
             player.currency[color] -= 1
             self.board.coins[color] += 1
 
+    def take_double_coins(self, player, color_to_take, colors_to_return=None):
+        if colors_to_return is None:
+            colors_to_return = dict()
+
+        if player != self.players[self.player_turn]:
+            raise ValueError("It isn't this players turn")
+
+        if self.board.coins[color_to_take] < 4:
+            raise ValueError("There aren't at least 4 coins from the color you requested")
+
+        if not all([player.currency[color] >= amnt for color, amnt in colors_to_return.items()]):
+            raise ValueError("You do no own one or more of the colors you tried to return")
+
+        if len(player.currency) + 2 - len(colors_to_return) > 10:
+            raise ValueError("Your action brings you to more than 10 coins")
+
+        if GemColor.JOKER == color_to_take:
+            raise ValueError("Jokers can only be taken via mortgaging")
+
+        player.currency[color_to_take] += 2
+        self.board.coins[color_to_take] -= 2
+
+        for color in colors_to_return:
+            player.currency[color] -= 1
+            self.board.coins[color] += 1
