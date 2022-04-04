039504a409ba4e7e3ec3aeb30dd44b5df7961a6f
Timo Pomer <timopomer@gmail.com>
2022-04-04 21:31:37 +0300

    fix main

--- splendor_ai/main.py
+++ splendor_ai/main.py
@@ -1,8 +1,4 @@
-from splendor_ai.entities.card import Card
-from splendor_ai.entities.gem_color import GemColor
+import constants
 
 if __name__ == '__main__':
-
-    l = [Card(0,GemColor.RED,0,0,0,0,0,0)]
-    print(l)
-
+    print(constants.FULL_DECK)
