611f3ed00e431651742de890fa390a8cb54ec9db
Timo Pomer <timopomer@gmail.com>
2022-04-04 23:18:38 +0300

    add board

---
+++ splendor_ai/game/board.py
@@ -0,0 +1,37 @@
+import itertools
+import random
+from collections import defaultdict
+from typing import List
+
+from splendor_ai import constants
+from splendor_ai.entities.card import Card
+from splendor_ai.entities.gem_color import GemColor
+from splendor_ai.entities.noble import Noble
+
+
+class Board:
+    def __init__(self, num_players: int, cards: List[Card] = constants.FULL_DECK,
+                 nobles: List[Noble] = constants.NOBLES):
+        num_nobles = num_players + 1
+        nobles_selection = nobles.copy()
+        random.shuffle(nobles_selection)
+        self._nobles = nobles_selection[:num_nobles]
+
+        cards_selection = cards.copy()
+        random.shuffle(cards_selection)
+        self.decks = defaultdict(list)
+        for card in cards:
+            self.decks[card.level].append(card)
+
+        players_to_coins = {4: 7, 3: 5, 2: 4}
+        num_coins = players_to_coins[num_players]
+
+        # Initialize game parameters
+        self.coins = {
+            GemColor.WHITE: num_coins,
+            GemColor.RED: num_coins,
+            GemColor.GREEN: num_coins,
+            GemColor.BLUE: num_coins,
+            GemColor.BLACK: num_coins,
+            GemColor.JOKER: 5
+        }
--- splendor_ai/game/game.py
+++ splendor_ai/game/game.py
@@ -1,5 +1,6 @@
 from splendor_ai.entities.gem_color import GemColor
 from splendor_ai.constants import NOBLES, FULL_DECK
+from splendor_ai.game.board import Board
 from splendor_ai.game.errors import IncorrectNumPlayersError
 import numpy as np
 
@@ -12,24 +13,6 @@ class Game:
             raise IncorrectNumPlayersError()
 
         # Define currency and nobles according to number of players
-        num_nobles = num_players + 1
-        if num_players == 4:
-            num_coins = 7
-        elif num_players == 3:
-            num_coins = 5
-        elif num_players == 2:
-            num_coins = 4
 
-        # Initialize game parameters
-        self.coins = {GemColor.WHITE: num_coins, GemColor.RED: num_coins,
-                      GemColor.GREEN: num_coins, GemColor.BLUE: num_coins,
-                      GemColor.BLACK: num_coins, GemColor.JOKER: 5}
-        self.deck = FULL_DECK
-        self.nobles = NOBLES
-
-        # Shuffle deck and nobles
-        self.random_seed = np.random.randint(0, 10000000)
-        np.random.shuffle(self.deck)
-        np.random.shuffle(self.nobles)
-        self.nobles = self.nobles[:num_nobles]
+        self.board = Board(num_players)
 
