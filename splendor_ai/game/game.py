83446316921d5780bbdf5ad7ff5d1a705627f613
Omer Belsky <omerbelsky3@gmail.com>
2022-04-04 22:42:52 +0300

    added init for game class

---
+++ splendor_ai/game/game.py
@@ -0,0 +1,40 @@
+from splendor_ai.entities.gem_color import GemColor
+from splendor_ai.constants import NOBLES, FULL_DECK
+import numpy as np
+
+
+class IncorrectNumPlayersError(Exception):
+    def __init__(self):
+        self.message = "Too many players. Player count should be an integer between 2 and 4."
+
+
+class Splendor:
+
+    def __init__(self, num_players):
+
+        # Make sure number of players is valid
+        if num_players not in [2, 3, 4]:
+            raise IncorrectNumPlayersError()
+
+        # Define currency and nobles according to number of players
+        num_nobles = num_players + 1
+        if num_players == 4:
+            num_coins = 7
+        elif num_players == 3:
+            num_coins = 5
+        elif num_players == 2:
+            num_coins = 4
+
+        # Initialize game parameters
+        self.coins = {GemColor.WHITE: num_coins, GemColor.RED: num_coins,
+                      GemColor.GREEN: num_coins, GemColor.BLUE: num_coins,
+                      GemColor.BLACK: num_coins, GemColor.JOKER: 5}
+        self.deck = FULL_DECK
+        self.nobles = NOBLES
+
+        # Shuffle deck and nobles
+        self.random_seed = np.random.randint(0, 10000000)
+        np.random.shuffle(self.deck)
+        np.random.shuffle(self.nobles)
+        self.nobles = self.nobles[:num_nobles]
+
