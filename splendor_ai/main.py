c3a79dbd83d4c1cc69e502bebca71fbf42e6afcb
Timo Pomer <timopomer@gmail.com>
2022-04-04 23:18:38 +0300

    add board

--- splendor_ai/main.py
+++ splendor_ai/main.py
@@ -1,7 +1,11 @@
 from splendor_ai.entities.gem_color import GemColor
+from splendor_ai.game.board import Board
+from splendor_ai.game.game import Game
 from splendor_ai.game.player import Player
 
 if __name__ == '__main__':
     player = Player()
     player.currency[GemColor.BLACK] += 1
     print(player)
+    g = Game(4)
+    print(g)
