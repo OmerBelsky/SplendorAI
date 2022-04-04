ea075d45e22b08984f0e2f01606f64d002d1ecce
Timo Pomer <timopomer@gmail.com>
2022-04-04 21:19:43 +0300

    added entities

---
+++ splendor_ai/main.py
@@ -0,0 +1,8 @@
+from splendor_ai.entities.card import Card
+from splendor_ai.entities.gem_color import GemColor
+
+if __name__ == '__main__':
+
+    l = [Card(0,GemColor.RED,0,0,0,0,0,0)]
+    print(l)
+
