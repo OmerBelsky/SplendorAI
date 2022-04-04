2cb6a9b8ffc7f966c2786fbbe7ccd7f9da78c9d0
Omer Belsky <omerbelsky3@gmail.com>
2022-04-04 22:39:29 +0300

    added init for game class

--- splendor_ai/main.py
+++ splendor_ai/main.py
@@ -1,7 +1,5 @@
-from splendor_ai.entities.gem_color import GemColor
-from splendor_ai.game.player import Player
+from splendor_ai.game.game import Splendor
 
 if __name__ == '__main__':
-    player = Player()
-    player.currency[GemColor.BLACK] += 1
-    print(player)
+    game = Splendor(3)
+    print(game.coins)
