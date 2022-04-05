e3e12738c28e2116122d94be6ec400b51e1670f3
Omer Belsky <omerbelsky3@gmail.com>
2022-04-05 20:10:17 +0300

    removed errors.py added logic to buy a card from the board

--- splendor_ai/game/game.py
+++ splendor_ai/game/game.py
@@ -1,7 +1,5 @@
 from splendor_ai.entities.gem_color import GemColor
-from splendor_ai.constants import NOBLES, FULL_DECK
 from splendor_ai.game.board import Board
-from splendor_ai.game.errors import IncorrectNumPlayersError
 import numpy as np
 
 class Game:
@@ -12,68 +10,104 @@ class Game:
         self.num_players = len(players)
         # Make sure number of players is valid
         if self.num_players not in [2, 3, 4]:
-            raise IncorrectNumPlayersError()
+            raise ValueError("Too many players. Player count should be an integer between 2 and 4.")
 
         # Define currency and nobles according to number of players
         self.board = Board(self.num_players)
         self.player_turn = 0
 
-    def take_three_coins(self, player, colors_to_take, colors_to_return=None):
+    def take_three_coins(self, player, coins_to_take, coins_to_return=None):
 
-        if colors_to_return is None:
-            colors_to_return = dict()
+        if coins_to_return is None:
+            coins_to_return = dict()
 
         if player != self.players[self.player_turn]:
             raise ValueError("It isn't this players turn")
 
-        if sum(colors_to_take.values()) > 3:
+        if sum(coins_to_take.values()) > 3:
             raise ValueError("At most 3 coins can be requested")
 
-        if any([amnt > 1 for amnt in colors_to_take.values()]):
+        if any([amnt > 1 for amnt in coins_to_take.values()]):
             raise ValueError("Coins requested must be unique")
 
-        if not all([self.board.coins[color] > 0 for color in colors_to_take]):
+        if not all([self.board.coins[color] > 0 for color in coins_to_take]):
             raise ValueError("One or more of the colors you requested is not available on the board")
 
-        if not all([player.currency[color] >= amnt for color, amnt in colors_to_return.items()]):
+        if not all([player.currency[color] >= amnt for color, amnt in coins_to_return.items()]):
             raise ValueError("You do no own one or more of the colors you tried to return")
 
-        if len(player.currency) + len(colors_to_take) - len(colors_to_return) > 10:
+        if len(player.currency) + len(coins_to_take) - len(coins_to_return) > 10:
             raise ValueError("Your action brings you to more than 10 coins")
 
-        if GemColor.JOKER in colors_to_take:
+        if GemColor.JOKER in coins_to_take:
             raise ValueError("Jokers can only be taken via mortgaging")
 
-        for color in colors_to_take:
+        for color in coins_to_take:
             player.currency[color] += 1
             self.board.coins[color] -= 1
 
-        for color, amnt in colors_to_return.items():
+        for color, amnt in coins_to_return.items():
             player.currency[color] -= amnt
             self.board.coins[color] += amnt
 
-    def take_double_coins(self, player, color_to_take, colors_to_return=None):
-        if colors_to_return is None:
-            colors_to_return = dict()
+        self.player_turn = (self.player_turn + 1) % self.num_players
+
+    def take_double_coins(self, player, coin_to_take, coins_to_return=None):
+        if coins_to_return is None:
+            coins_to_return = dict()
 
         if player != self.players[self.player_turn]:
             raise ValueError("It isn't this players turn")
 
-        if self.board.coins[color_to_take] < 4:
+        if self.board.coins[coin_to_take] < 4:
             raise ValueError("There aren't at least 4 coins from the color you requested")
 
-        if not all([player.currency[color] >= amnt for color, amnt in colors_to_return.items()]):
+        if not all([player.currency[color] >= amnt for color, amnt in coins_to_return.items()]):
             raise ValueError("You do no own one or more of the colors you tried to return")
 
-        if len(player.currency) + 2 - len(colors_to_return) > 10:
+        if len(player.currency) + 2 - len(coins_to_return) > 10:
             raise ValueError("Your action brings you to more than 10 coins")
 
-        if GemColor.JOKER == color_to_take:
+        if GemColor.JOKER == coin_to_take:
             raise ValueError("Jokers can only be taken via mortgaging")
 
-        player.currency[color_to_take] += 2
-        self.board.coins[color_to_take] -= 2
+        player.currency[coin_to_take] += 2
+        self.board.coins[coin_to_take] -= 2
 
-        for color, amnt in colors_to_return.items():
+        for color, amnt in coins_to_return.items():
             player.currency[color] -= amnt
             self.board.coins[color] += amnt
+
+        self.player_turn = (self.player_turn + 1) % self.num_players
+
+    def buy_card(self, player, level, idx, coins_to_pay):
+
+        if player != self.players[self.player_turn]:
+            raise ValueError("It isn't this players turn")
+
+        if level not in [1, 2, 3]:
+            raise ValueError("Card level must be an integer between 1 and 3")
+
+        if idx not in [1, 2, 3, 4]:
+            raise ValueError("Card index must be an integer between 1 and 4")
+
+        if any([coins_to_pay[color] > player.currency[color] for color in coins_to_pay]):
+            ValueError("You tried to pay with more coins than you own")
+
+        requested_card = self.board.decks[level][idx - 1]
+        diff_dict = {color: requested_card.price[color] - coins_to_pay[color] for color in self.board.coins}
+
+        if any([diff_dict[color] < 0 for color in diff_dict]):
+            raise ValueError("You over-payed a type of coin")
+
+        if sum(diff_dict.values()) > coins_to_pay[GemColor.JOKER]:
+            raise ValueError("You under-payed for the card")
+
+        for color, amnt in coins_to_pay.items():
+            player.currency[color] -= amnt
+            self.board.coins[color] += amnt
+
+        self.board.decks[level].pop(idx - 1)
+        player.cards.append(requested_card)
+
+        self.player_turn = (self.player_turn + 1) % self.num_players
