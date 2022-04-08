e24892b2e9c37406e91937b3c47210a8853c50c5
Omer Belsky <omerbelsky3@gmail.com>
2022-04-08 12:58:39 +0300

    divided the buy card function into 2 (mortgaged and buying from deck) as well as fixed flaws with buying with jokers (#13)

--- splendor_ai/entities/noble.py
+++ splendor_ai/entities/noble.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from typing import Dict
 
+from splendor_ai.entities.gem_color import GemColor
 
 @dataclass
 class Noble:
@@ -9,3 +11,14 @@ class Noble:
     req_g: int
     req_b: int
     point_value: int = 3
+
+
+    @property
+    def requirements(self) -> Dict[GemColor, int]:
+        return {
+            GemColor.WHITE: self.req_w,
+            GemColor.RED: self.req_r,
+            GemColor.BLUE: self.req_u,
+            GemColor.GREEN: self.req_g,
+            GemColor.BLACK: self.req_b
+        }
