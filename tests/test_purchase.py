df7c39c2316a34f5ee435ff03f4d03c426da14d8
Omer Belsky <omerbelsky3@gmail.com>
2022-04-08 14:26:31 +0300

    Game (#16)
    * divided the buy card function into 2 (mortgaged and buying from deck) as well as fixed flaws with buying with jokers
    
    * changed check functions to use get() instead of accessing colors directly in the coins_to_pay dictionaries
    
    * added tests for purchasing cards via the game

--- tests/test_purchase.py
+++ tests/test_purchase.py
@@ -43,8 +43,33 @@ def test_card_not_purchaseable_with_only_jokers_sanity():
     assert card.purchaseable_with({GemColor.JOKER: 10})
 
 
-def test_cant_cheat_coins():
+def test_cant_buy_without_paying():
     players = [Player() for _ in range(4)]
     game = Game(players)
     with pytest.raises(ValueError):
         game.buy_deck_card(players[game.player_turn], 1, 1, {color: 0 for color in GEM_COLORS})
+
+        
+def test_cant_buy_with_coins_not_owned():
+    players = [Player() for _ in range(4)]
+    game = Game(players)
+    with pytest.raises(ValueError):
+        card = game.board.decks[1][1]
+        game.buy_deck_card(players[game.player_turn], 1, 1, {color: card.price[color] for color in GEM_COLORS})
+
+        
+def test_card_purhcased():
+    players = [Player() for _ in range(4)]
+    game = Game(players)
+    card = game.board.decks[2][0]
+    for color in GEM_COLORS:
+        players[0].currency[color] += 100
+    game.buy_deck_card(players[game.player_turn], 2, 1, card.price)
+    assert game.player_turn == 1
+    assert card in players[0].cards
+    assert players[0].discounts[card.gem_color] == 1
+    assert players[0].points == card.point_value
+    assert all([players[0].currency[color] == (100 - card.price[color]) for color in GEM_COLORS])
+    assert card not in game.board.decks[2]
+    assert all([game.board.coins[color] == (7 + card.price[color]) for color in GEM_COLORS])
+
